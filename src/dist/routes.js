8478aa41b41ff7ae6e83b50c00f9ed908d88a5e6
Super <Kai.Tui@geely.com>
2022-09-21 16:30:21 +0800

    <feat>(导航守卫): 导航守卫
    -  导航守卫
    
    Kai.Tui

--- src/dist/routes.js
+++ src/dist/routes.js
@@ -5,15 +5,27 @@ var home_vue_1 = require("./pages/home.vue");
 var login_vue_1 = require("./pages/login.vue");
 var ColumnDetail_vue_1 = require("./pages/ColumnDetail.vue");
 var createPost_vue_1 = require("./pages/createPost.vue");
+var store_1 = require("./store");
 var routes = [
     { path: '/', name: 'home', component: home_vue_1["default"] },
-    { path: '/login', component: login_vue_1["default"] },
+    { path: '/login', name: 'login', component: login_vue_1["default"], meta: { redirectAlreadyLogin: true } },
     { path: '/columnDetail/:id', name: 'columnDetail', component: ColumnDetail_vue_1["default"] },
-    { path: '/create', name: 'create', component: createPost_vue_1["default"] }
+    { path: '/create', name: 'create', component: createPost_vue_1["default"], meta: { requiredLogin: true } }
 ];
 var routerHistory = vue_router_1.createWebHistory();
 var router = vue_router_1.createRouter({
     history: routerHistory,
     routes: routes
+});
+router.beforeEach(function (to, from, next) {
+    if (to.meta.requiredLogin && !store_1["default"].state.user.isLogin) {
+        next({ name: 'login' });
+    }
+    else if (to.meta.redirectAlreadyLogin && store_1["default"].state.user.isLogin) {
+        next({ name: 'home' });
+    }
+    else {
+        next();
+    }
 });
 exports["default"] = router;
--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,30 @@
-import {createWebHistory,createRouter} from 'vue-router'
+import { createWebHistory, createRouter, useRoute } from 'vue-router';
 import Home from './pages/home.vue'
 import Login from './pages/login.vue'
 import columnDetail from "./pages/ColumnDetail.vue";
 import createPost from "./pages/createPost.vue";
+import store from './store';
 const routes = [
   { path: '/',name:'home', component: Home },
-  { path: '/login', component: Login },
+  { path: '/login',name:'login', component: Login,meta:{redirectAlreadyLogin:true}},
   { path: '/columnDetail/:id',name:'columnDetail', component: columnDetail },
-  { path: '/create',name:'create', component: createPost }
+  { path: '/create',name:'create', component: createPost,meta:{requiredLogin:true}}
 ]
+
+
 const routerHistory = createWebHistory()
 const router = createRouter({
   history:routerHistory,
   routes: routes
 })
-
+router.beforeEach((to,from,next) => {
+ if (to.meta.requiredLogin && !store.state.user.isLogin) {
+    next({name:'login'})
+ }else if(to.meta.redirectAlreadyLogin && store.state.user.isLogin){
+  next({name:'home'})
+ }
+ else {
+  next()
+ }
+})
 export default router
